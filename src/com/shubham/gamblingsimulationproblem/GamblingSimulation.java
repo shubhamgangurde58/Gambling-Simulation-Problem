bdde4495103a35cfd7c1ae682fb900a25cf67459
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-05 19:26:50 +0530

    [MODIFIED]:shubham checking per day stack on bit per game

--- src/com/shubham/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/shubham/gamblingsimulationproblem/GamblingSimulation.java
@@ -2,10 +2,18 @@ package com.shubham.gamblingsimulationproblem;
 
 public class GamblingSimulation {
 
-	public static void main(String[] args) {
+		private static  final int STACK_PER_DAY= 100;
+		private static final int BIT_PER_GAME = 1;
+    
+		public static void gameStatus() {
+    	  System.out.println("STACK_PER_DAY = "+STACK_PER_DAY);
+    	  System.out.println("BIT_PER_GAME = "+BIT_PER_GAME);
+		}
+    
+		public static void main(String[] args) {
 		
-		System.out.println("Welcome to Gambling Simulation Problem");
+			System.out.println("Welcome to Gambling Simulation Problem");
 		
-	}
-
+			gameStatus();
+		}
 }
