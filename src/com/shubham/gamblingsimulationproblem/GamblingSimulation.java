99ef853b313c2ad5cc93b4bf5470272d502381f0
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-05 19:04:01 +0530

    [ADDED]:shubham Welcome message for Gambling Simulation Problem

---
+++ src/com/shubham/gamblingsimulationproblem/GamblingSimulation.java
@@ -0,0 +1,11 @@
+package com.shubham.gamblingsimulationproblem;
+
+public class GamblingSimulation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Gambling Simulation Problem");
+		
+	}
+
+}
